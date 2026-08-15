09fb9f1544cba2dde0a2999ceddb7ae01c577cbf
workingbee74 <stlouiskev@gmail.com>
2026-08-15 16:53:24 -0500

    Refactor product assignment logic in app.py

--- app.py
+++ app.py
@@ -28,18 +28,18 @@ def parse_card_title(title):
         manufacturer = "Panini"
     # Product
     product = None
-   if "BOWMAN STERLING" in title_upper or "STERLING" in title_upper:
-    product = "Bowman Sterling"
-   elif "BOWMAN CHROME" in title_upper:
-    product = "Bowman Chrome"
-   elif "BOWMAN DRAFT" in title_upper:
-    product = "Bowman Draft"
-   elif "TOPPS CHROME" in title_upper:
-    product = "Topps Chrome"
-   elif "BOWMAN" in title_upper:
-    product = "Bowman"
-   elif "TOPPS" in title_upper:
-    product = "Topps"
+    if "BOWMAN STERLING" in title_upper or "STERLING" in title_upper:
+     product = "Bowman Sterling"
+    elif "BOWMAN CHROME" in title_upper:
+     product = "Bowman Chrome"
+    elif "BOWMAN DRAFT" in title_upper:
+     product = "Bowman Draft"
+    elif "TOPPS CHROME" in title_upper:
+     product = "Topps Chrome"
+    elif "BOWMAN" in title_upper:
+     product = "Bowman"
+    elif "TOPPS" in title_upper:
+     product = "Topps"
        # Player name
     player_name = None
 
