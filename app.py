8d87d050ad1656eaec7370c342c4080040d85a56
workingbee74 <stlouiskev@gmail.com>
2026-08-16 16:47:34 -0500

    Remove card_number null check from SQL query
    Removed condition to check for non-null card_number in query.

--- app.py
+++ app.py
@@ -771,7 +771,6 @@ def deals_dashboard_v2():
                     WHERE
                         is_single_card = TRUE
                         AND player_name IS NOT NULL
-                        AND card_number IS NOT NULL
                         AND asking_price IS NOT NULL
                     GROUP BY
                         player_name,
