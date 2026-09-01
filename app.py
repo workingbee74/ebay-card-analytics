cd07491ca2d9fc9036ecdbab4496f067f54b9aa7
workingbee74 <stlouiskev@gmail.com>
2026-08-31 19:34:06 -0500

    Initialize fallback_prices in app.py
    Add fallback_prices initialization before checking exact_prices.

--- app.py
+++ app.py
@@ -3823,7 +3823,9 @@ def auction_value_refresh():
                     )
 
                     exact_prices = price_tiers["exact_prices"]
-                
+
+                    fallback_prices = []
+                    
                     if not exact_prices:
                         exact_prices = price_tiers["same_parallel_prices"]
                     
